54d3e148f8e0a8a9554a10187ea04ad1655aa6f0
Naichen <49353868+naichenzhao@users.noreply.github.com>
2022-01-30 22:14:53 -0800

    more progress, issues with material contact

--- PyMAPDL Test/CTE Test/main.py
+++ PyMAPDL Test/CTE Test/main.py
@@ -25,6 +25,15 @@ def main():
 
 
 def create_volume(mapdl):
+
+    def meshVolume(vol, mat):
+        mapdl.mat(mat)
+        mapdl.geometry.volume_select(vol, 'S')
+        mapdl.et(1, 'SOLID187')
+        mapdl.vmesh(vol)
+        mapdl.geometry.volume_select(vol, 'U')
+
+
     print("Creating Volume... ")
     mapdl.prep7()
 
@@ -40,22 +49,37 @@ def create_volume(mapdl):
 
 
     # Create object 1
-    mapdl.mat(1)
-    mapdl.block(-1, 0, 0, 10, -1, 1)
+    mapdl.block(9, 11, 0, 10, -1, 1)
     v1 = mapdl.geometry.vnum[0]
     print(mapdl.geometry.vnum)
-    mapdl.et(1, 'SOLID5')
-    mapdl.vmesh(v1)
     mapdl.geometry.volume_select(v1, 'U')
 
     # Create Object 2
-    mapdl.mat(2)
-    mapdl.block(0, 1, 0, 10, -1, 1)
+    mapdl.block(-1, 1, 0, 10, -1, 1)
     v2 = mapdl.geometry.vnum[0]
     print(mapdl.geometry.vnum)
-    mapdl.et(1, 'SOLID5')
-    mapdl.vmesh(v2)
     mapdl.geometry.volume_select(v2, 'U')
+
+
+    # Create object case
+    mapdl.block(-3, -2, 0, 10, -1, 1)
+    mapdl.block(2, 3, 0, 10, -1, 1)
+
+    mapdl.block(-3, 3, -1, -0, -1, 1)
+    mapdl.block(-3, 3, 10, 11, -1, 1)
+    mapdl.vplot()
+
+    mapdl.vadd(*mapdl.geometry.vnum)
+    v3 = mapdl.geometry.vnum[0]
+    print(mapdl.geometry.vnum)
+    mapdl.vplot()
+    mapdl.geometry.volume_select(v3, 'U')
+   
+
+    
+    meshVolume(v3, 1)
+    meshVolume(v1, 2)
+    meshVolume(v2, 2)
 
     print("Volume Processing Done... \n")
     
@@ -90,8 +114,8 @@ def run(mapdl):
 
     print("displaying values:\n")
 
-    di = mapdl.post_processing.nodal_displacement('Y')
-    print(di)
+    # di = mapdl.post_processing.nodal_displacement('Y')
+    # print(di)
 
 
     print("Finished Running")
